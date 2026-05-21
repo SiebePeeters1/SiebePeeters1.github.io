d2b5f975fac5f83898d2740e76301009d27371c6
Siebe Peeters <s151605@ap.be>
2026-05-21 20:51:59 +0200

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-const contactButton = document.getElementById('contactButton');
-
-if (contactButton) {
-  contactButton.addEventListener('click', () => {
-    alert('Thanks for visiting! Contact details coming soon.');
-  });
-}
