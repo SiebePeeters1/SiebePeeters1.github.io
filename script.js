0523d6dd935607c1a3e2cd7c9d5bc7adb1905f5d
Siebe Peeters <s151605@ap.be>
2026-05-21 20:48:00 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+const contactButton = document.getElementById('contactButton');
+
+if (contactButton) {
+  contactButton.addEventListener('click', () => {
+    alert('Thanks for visiting! Contact details coming soon.');
+  });
+}
